d86a52e51b83ab444bf5633964c35392262e297c
Fikri Ramadianto <ramadiantofikri@gmail.com>
2023-05-21 23:59:01 +0700

    layout-checkout-page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,9 +6,15 @@ import "./cart-dropdown.styles.scss";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate()
+
+  const checkoutHandler = () => {
+    navigate("/checkout")
+  }
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,7 +22,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>Checkout</Button>
+      <Button onClick={checkoutHandler}>Checkout</Button>
     </div>
   );
 };
